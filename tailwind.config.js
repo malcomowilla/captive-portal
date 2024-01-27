512586be3ac964b97708a0f96ec1f79ba420ebf8
malcomowilla <owillamalcom@gmail.com>
2024-01-27 08:46:31 +0300

    changing appaearance4

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,9 @@ export default {
         montserrat:['Montserrat', 'sans-serif']
       }
     },
-    daisyui: {
-      themes: ['light', 'dark']
-     },
+    // daisyui: {
+    //   themes: ['light', 'dark']
+    //  },
   },
   plugins: [
     require('@shrutibalasa/tailwind-grid-auto-fit'),
